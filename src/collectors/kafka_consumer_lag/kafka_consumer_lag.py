2c51e7148463f671ab18e9ba844adcb5698c3139
WEI WU <wei@teambanjo.com>
2016-03-09 15:11:49 -0800

    -add kafka consumer lag collector

--- src/collectors/kafka_consumer_lag/kafka_consumer_lag.py
+++ src/collectors/kafka_consumer_lag/kafka_consumer_lag.py
@@ -13,6 +13,36 @@ using ConsumerOffsetChecker.
 import diamond.collector
 
 
+class ConsumerMetric(object):
+    def __init__(self, cluster_name, metrics):
+        super(ConsumerMetric, self).__init__()
+        self.cluster_name = cluster_name
+        self.consumer_group, \
+        self.topic, \
+        self.partition, \
+        self.consumer_offset, \
+        self.broker_offset, \
+        self.consumer_lag, \
+        _ = metrics
+
+    def get_consumer_lag_metric_name(self):
+        if self.cluster_name:
+            prefix_keys = [self.cluster_name, self.consumer_group, self.topic, self.partition]
+        else:
+            prefix_keys = [self.consumer_group, self.topic, self.partition]
+        return '.'.join(prefix_keys)
+
+    def get_total_lag_metric_name(self):
+        if self.cluster_name:
+            prefix_keys = [self.cluster_name, self.consumer_group, self.topic, 'total']
+        else:
+            prefix_keys = [self.consumer_group, self.topic, 'total']
+        return '.'.join(prefix_keys)
+
+    def get_consumer_lag(self):
+        return int(self.consumer_lag)
+
+
 class KafkaConsumerLagCollector(diamond.collector.ProcessCollector):
 
     def get_default_config_help(self):
@@ -79,6 +109,7 @@ class KafkaConsumerLagCollector(diamond.collector.ProcessCollector):
 
         topics = self.get_topics(zookeeper)
         for topic in topics:
+            consumer_metrics = []
             for consumer_group in consumer_groups:
                 try:
                     cmd = [
@@ -105,11 +136,19 @@ class KafkaConsumerLagCollector(diamond.collector.ProcessCollector):
                         if not metrics:
                             continue
 
-                        prefix_keys = metrics[:3]
-                        value = float(metrics[5])
+                        metric = ConsumerMetric(cluster_name, metrics)
+                        consumer_lag_metric_name = metric.get_consumer_lag_metric_name()
+                        self.publish(consumer_lag_metric_name, metric.get_consumer_lag())
 
-                        if cluster_name:
-                            prefix_keys.insert(0, cluster_name)
-                        self.publish('.'.join(prefix_keys), value)
+                        consumer_metrics.append(metric)
                 except Exception as e:
                     self.log.error(e)
+
+            if consumer_metrics:
+                total_lag = 0
+                total_lag_metric_name = None
+                for consumer_metric in consumer_metrics:
+                    if total_lag_metric_name is None:
+                        total_lag_metric_name = consumer_metric.get_total_lag_metric_name()
+                    total_lag += consumer_metric.get_consumer_lag()
+                self.publish(total_lag_metric_name, total_lag)
