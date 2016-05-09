ea6baf55a16b1df69dc910e2cdb319407768abd6
Wei Wu <wei@teambanjo.com>
2016-05-09 13:19:48 -0700

    fix metric name includes `/` and `:` fix metric value with bool type

--- src/collectors/mongodb/mongodb.py
+++ src/collectors/mongodb/mongodb.py
@@ -340,12 +340,14 @@ class MongoDBCollector(diamond.collector.Collector):
             return
         value = data[key]
         keys = prev_keys + [key]
-        keys = [x.replace(" ", "_").replace("-", ".") for x in keys]
+        keys = [x.replace(' ', '_').replace('/', '-').replace(',', '-').replace(':', '-') for x in keys]
         if not publishfn:
             publishfn = self.publish
         if isinstance(value, dict):
             for new_key in value:
                 self._publish_metrics(keys, new_key, value)
+        elif isinstance(value, bool):
+            publishfn('.'.join(keys), int(value))
         elif isinstance(value, int) or isinstance(value, float):
             publishfn('.'.join(keys), value)
         elif isinstance(value, long):
